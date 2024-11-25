e4381b953868dce973b3d8ae8de7d1407b9ce05a
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-11-25 12:53:47 +0100

    build friendsList comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friendObj={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friendObj }) {
+  return (
+    <li>
+      <img src={friendObj.image} alt={friendObj.name} />
+      <h3>{friendObj.name}</h3>
+
+      {friendObj.balance < 0 && (
+        <p className="red">
+          You owe {friendObj.name} ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance > 0 && (
+        <p className="green">
+          {friendObj.name} owes you ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance === 0 && <p>You and {friendObj.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
