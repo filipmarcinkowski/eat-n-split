7a63fe1fb597d82bcca02a8186e1fe45b55a9617
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-11-25 14:21:27 +0100

    add split money comp

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add fiend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -74,9 +75,34 @@ function FormAddFriend({}) {
       <label>🙋‍♂️ Friend name</label>
       <input type="text" />
 
-      <label>🖼Image URL</label>
+      <label>🖼 Image URL</label>
       <input type="text" />
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♂️ Your expense</label>
+      <input type="text" />
+
+      <label>👫 X's expense </label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
